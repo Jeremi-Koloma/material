e037cdfb715b46b472990590c9e6fa6cd56aeaf9
Jeremi-Koloma <kolomajeremi60@gmail.com>
2024-08-21 23:02:00 +0000

    MAT-CARD OKAY

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,19 +4,33 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule  } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+
+// Routes for Customers sections
 const routes: Routes = [
-  { path: '', component: CustomersListComponent }
+  { path: '', component: CustomersListComponent },
+  { path: 'infos', component: CustomerCardComponent },
 ]
 
 @NgModule({
   declarations: [
-    CustomersListComponent
+    CustomersListComponent,
+    CustomerCardComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    // Material
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
   ]
 })
 export class CustomersModule { }
